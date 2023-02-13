a75f35396eda0ea2148904adb83bd2a9b832fb56
monccciii <sook361@yahoo.com>
2023-02-12 23:28:18 -0500

    created /createform route + reusable MongoDB connection :)

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
@@ -19,37 +25,71 @@ type Form struct {
 type Question struct {
     Text string `json:"text"`
 }
+func connectMongo() (*mongo.Client, context.Context, context.CancelFunc, error) {
+	// Load the environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 
+	// Get the MongoDB URI from the environment variables
+	uri := os.Getenv("MONGODB_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, nil, nil, err
+	}
 
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// Connect to the MongoDB database
+	err = client.Connect(ctx)
+	if err != nil {
+		cancel()
+		return nil, nil, nil, err
+	}
+
+	return client, ctx, cancel, nil
+}
 
 func createForm(c *gin.Context) {
-    // Parse the request body into a Form struct
-    var form Form
-    if err := json.NewDecoder(c.Request.Body).Decode(&form); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    // Generate a unique ID for the form
-    form.ID = uuid.New().String()
-    // Print out the form data
-    fmt.Println("Form Data:")
-    fmt.Printf("ID: %s\n", form.ID)
-    fmt.Printf("Username: %s\n", form.Creator)
-    for i, question := range form.Questions {
-        fmt.Printf("Question %d: %s\n", i + 1, question)
-    }
-    // Process the form data, for example by storing it in a database
-    // ...
-    // Return a response to the client indicating success
-    c.JSON(http.StatusOK, gin.H{"success": "form processed"})
+	// Connect to the MongoDB database
+	client, ctx, cancel, err := connectMongo()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer client.Disconnect(ctx)
+	defer cancel()
+
+	// Parse the request body into a Form struct
+	var form Form
+	if err := json.NewDecoder(c.Request.Body).Decode(&form); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Generate a unique ID for the form
+	form.ID = uuid.New().String()
+
+	// Get a handle to the "graphforms" database and "form" collection
+	formsCollection := client.Database("graphforms").Collection("forms")
+
+	// Insert the form struct into the "form" collection
+	_, err = formsCollection.InsertOne(ctx, form)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return a response to the client indicating success
+	c.JSON(http.StatusOK, gin.H{"success": "form created"})
 }
+
 
 
 
 func main() {
     router := gin.Default()
-    router.GET("/albums", getAlbums)
-	router.GET("/albums2", getAlbums2)
 	router.POST("/createForm", createForm)
 
     router.Run("localhost:8080")
