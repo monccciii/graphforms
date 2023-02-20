5920f82bca30ccfb395249f99698d7778d47ca51
monccciii <sook361@yahoo.com>
2023-02-20 02:03:08 -0500

    forgot to push error fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const withDotenv = require('next-env')
-
-const withEnv = withDotenv()
-
-module.exports = withEnv({
-  env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-})
+module.exports = nextConfig
