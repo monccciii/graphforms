ddb530f5bbd049c22da8620a956db5e7b4898de9
monccciii <sook361@yahoo.com>
2023-02-20 23:35:42 -0500

    removing .env checks for the time being

--- backend/app/setup.go
+++ backend/app/setup.go
@@ -5,18 +5,11 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/monccciii/graphforms/db"
 	"github.com/monccciii/graphforms/router"
 )
 
 func SetupApp() error {
-	err := godotenv.Load()
-	if err != nil {
-    	return err
-	}
-
-
 	client, ctx, cancel, err := db.ConnectMongo()
 	if err != nil {
 		return err
