417e02568623f0c35e894c2e07e613d70ecea922
monccciii <sook361@yahoo.com>
2023-03-11 15:55:20 -0500

    lint error fix

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Landing() {
               <div className='sm:flex items-center'>
                 <div className='text-left mx-auto w-[30vw]'>
                   <p className='text-xl sm:text-2xl font-medium'>Real-time Data Visualization</p>
-                  <p className='mt-[1vh] font-regular text-[#444444] text-[18px]'>Easily visualize your data using GraphForms’ online tool. No need to use any programs or input data into a tool. GraphForms takes care of it from start to finish.</p>
+                  <p className='mt-[1vh] font-regular text-[#444444] text-[18px]'>Easily visualize your data using GraphForms&apos; online tool. No need to use any programs or input data into a tool. GraphForms takes care of it from start to finish.</p>
                 </div>
                 <div className='mx-auto'>
                   <Image src='/fillerimg.png' alt='' height={250} width={500}/>
